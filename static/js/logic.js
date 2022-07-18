1acc740549a6835f9a67fa46318f62a35c7983ae
Nicholas McMahon (Corporate) <NMcMahon@nsrltd.com>
2022-07-18 09:54:01 +0800

    final coding added

--- static/js/logic.js
+++ static/js/logic.js
@@ -68,3 +68,59 @@ function createFeatures(earthquakeData, plateData) {
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes, plates);
 }
+
+function createMap(earthquakes, plates) {
+
+    // Define basemap layers
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl0dGxlc3Rkb2xsIiwiYSI6ImNqZHdnbTBzYTQ3bXUyeG80ZTQ3dWJtNjIifQ.uvSL6xgyBBXQSJ1Yopx9gA");
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl0dGxlc3Rkb2xsIiwiYSI6ImNqZHdnbTBzYTQ3bXUyeG80ZTQ3dWJtNjIifQ.uvSL6xgyBBXQSJ1Yopx9gA");
+    
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl0dGxlc3Rkb2xsIiwiYSI6ImNqZHdnbTBzYTQ3bXUyeG80ZTQ3dWJtNjIifQ.uvSL6xgyBBXQSJ1Yopx9gA");
+
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap,
+        "Satellite": satellitemap
+    };
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        Plates: plates,
+        Earthquakes: earthquakes
+    };
+
+    // Create our map, giving it the darkmap, earthquakes, and plates layers to display on load
+    var myMap = L.map("map", {
+        center: [34.052235, -119.243683],
+        zoom: 4.5,
+        layers: [streetmap, plates, earthquakes]
+    });
+
+    var legend = L.control({position: 'bottomright'});
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control
+        .layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(myMap);
+    
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [color0, color1, color2, color3, color4, color5],
+            labels = ["0-1", "1-2","2-3", "3-4", "4-5", "5+"]
+        
+            // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + grades[i] + '"></i> ' + labels[i] + '<br>';
+        }
+        
+        return div;
+    };
+        
+    legend.addTo(myMap);    
+}
